6ca9b629097645d5e4aff1476e200df88e38a0d5
Matthew Hall <mdhall@gmail.com>
2016-07-02 15:37:46 -0500

    Add eslint and clean up source

---
+++ .eslintrc.js
@@ -0,0 +1,96 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "consistent-return": [
+            "error"
+        ],
+        "curly": [
+            "error",
+            "all"
+        ],
+        "no-sequences": [
+            "error"
+        ],
+        "no-throw-literal": [
+            "error"
+        ],
+        "no-shadow": [
+            "error",
+            { "builtinGlobals": true,  "allow": ['callback', 'err'] }
+        ],
+        "brace-style": [
+            "error",
+            "1tbs"
+        ],
+        "eqeqeq": [
+            "error",
+            "smart"
+        ],
+        "camelcase": [
+            "error",
+            {"properties": "never"}
+        ],
+        "comma-style": [
+            "error",
+            "last"
+        ],
+        "consistent-this": [
+            "error",
+            "_self"
+        ],
+        "eol-last": [
+            "error",
+        ],
+        "key-spacing": [
+            "error",
+            { "beforeColon": false, "afterColon": true }
+        ],
+        "keyword-spacing": [
+            "error",
+            { "before": true, "after": true }
+        ],
+        "new-parens": [
+            "error",
+        ],
+        "no-trailing-spaces": [
+            "error",
+        ],
+        "one-var": [
+            "error",
+            "never"
+        ],
+        "one-var-declaration-per-line": [
+            "error",
+            "always"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+        "space-infix-ops": [
+            "error",
+        ]
+    }
+};
