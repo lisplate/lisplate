7dd805d0389eb5091c084778b57e0f0254f1c40f
Matthew Hall <mdhall@gmail.com>
2016-07-08 11:44:53 -0500

    Fix bug with safe not applying to function calls

---
+++ spec/template-data/unescaped.js
@@ -0,0 +1,8 @@
+module.exports = {
+  fn: function(param) {
+    return "<br>";
+  },
+  escape: "<br>",
+  js: "var somejs = 'test';\nvar something = \"el\\\"se\";",
+  json: "{\"some\": \"json\"}"
+}
